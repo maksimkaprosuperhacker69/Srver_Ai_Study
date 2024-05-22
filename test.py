5144e4f5ce71f2ea0b1033d69bd9d737023f2a63
maksimkaprosuperhacker69 <120910365+maksimkaprosuperhacker69@users.noreply.github.com>
2024-05-22 19:41:21 +0500

    j

--- test.py
+++
@@ -1,2 +0,0 @@
-import urllib.request
-urllib.request.urlretrieve("https://cloud.appwrite.io/v1/storage/buckets/663c9a3500271995569d/files/6646fa4d5bf70c583c84/view?project=663c968d0034314f646f", 'name.png')
