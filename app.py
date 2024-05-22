b42c4a3f05d62b765050c8d045ae4e9049df7d88
maksimkaprosuperhacker69 <120910365+maksimkaprosuperhacker69@users.noreply.github.com>
2024-05-22 19:03:03 +0500

    commit

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+
+import AI
+from flask import Flask
+import urllib.request
+import file_reader_text
+
+app = Flask(__name__)
+
+
+@app.route("/ans/")
+def read_item(promt_response: str, course_id: str, file_path: str, que_count: int = 10, ):
+    name = f'{course_id}.png'
+    urllib.request.urlretrieve(file_path, name)
+
+    text = file_reader_text.text_reader(name)
+    os.remove(name)
+
+    return AI.main_gpt_ask(promt = AI.promt_create(promt_type = promt_response, que_count = que_count), text = text)
+
+app.run()
